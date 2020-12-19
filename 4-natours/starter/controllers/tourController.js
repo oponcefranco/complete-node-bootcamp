35d14ba89f21ff9abec473603a31f07aaa9c82cb
oponcefranco <poncefranco@gmail.com>
2020-12-19 09:37:15 -0800

    Chapter 8: add field limiting param

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -9,17 +9,34 @@ exports.getAllTours = async (req, res) => {
     try {
         console.log(req.query);
         // BUILD QUERY
-        // 1) filtering
+        // 1A) filtering
         const queryObj = {...req.query};
         const excludedFields = [ 'page', 'sort', 'limit', 'fields' ];
         excludedFields.forEach(el => delete queryObj[el]);
 
-        // 2) Advanced filtering
+        // 1B) Advanced filtering
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
         console.log(JSON.parse(queryStr));
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        // 3) Field Limiting
+        if (req.query.fields) {
+           const fields = req.query.fields.split(',').join(' ');
+           query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
         // EXECUTE QUERY
         const tours = await query;
 
